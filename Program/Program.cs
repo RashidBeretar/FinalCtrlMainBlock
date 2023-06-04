c5d6075d31b1d373ae8e67d9935aaba875c21c9b
Rashid <rashidberetar@gmail.com>
2023-06-04 16:34:20 +0300

    внёс основной алгоритм

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,3 +4,29 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+// var array_input = {“Hello”, “2”, “world”, “:-)”};
+
+int result_array_length = 0;
+int j = 0;
+
+string[] array_input = { "Hello", "2", "world", ":-)" };
+
+for (int i = 0; i < array_input.Length; i++)
+{
+    if (array_input[i].Length <= 3)
+    {
+        result_array_length++;
+    }
+}
+
+var result_array = new string[result_array_length];
+
+for (int i = 0; i < array_input.Length; i++)
+{
+    if (array_input[i].Length <= 3)
+    {
+        result_array[j] = array_input[i];
+        j++;
+    }
+}
